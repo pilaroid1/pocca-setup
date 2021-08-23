a4528afcc9820955384e2092c85431619652f301
Rémi Sarrailh <remi@madnerd.org>
2021-08-23 09:30:19 +0200

    Add secrets

---
+++ info.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append("/media/usb/apps")
+from pocca.utils.system import System
+
+system = System()
+#print(system.info)
+
+print (system.info)
--- setup.py
+++ setup.py
@@ -1,58 +1,57 @@
 import sys
-import configparser
+sys.path.append("/media/usb/apps")
+from pocca.utils.app import App
 
-sys.path.append("../")
-from pocca.utils.secrets import Secrets
-from pocca.utils.system import System
+app = App()
+app.clear_terminal()
 
-settings = configparser.ConfigParser()
-system = System()
-secrets = Secrets()
+wifi_name = app.secrets["WIFI"]["name"]
+user_name = app.secrets["USER"]["name"]
+name = app.settings["APPLICATION"]["name"]
 
-settings.read("/media/usb/secrets.ini")
-wifi_name = settings["WIFI"]["name"]
-user_name = settings["USER"]["name"]
-name = settings["USER"]["host"]
-
-if(settings["ENCRYPT"]["enable"] == "true"):
-  wifi_password = secrets.decode(settings["WIFI"]["password"])
+if(app.settings["APPLICATION"]["encrypt"] == "1"):
+  wifi_password = app.decoder.decode(app.secrets["WIFI"]["password"])
   if wifi_password is False:
     print(".... Encrypt WiFi Password ....")
-    wifi_password = settings["WIFI"]["password"]
-    settings["WIFI"]["password"] = secrets.encode(settings["WIFI"]["password"])
+    wifi_password = app.secrets["WIFI"]["password"]
+    app.secrets["WIFI"]["password"] = app.decoder.encode(app.secrets["WIFI"]["password"])
 
-  user_password = secrets.decode(settings["USER"]["password"])
+  user_password = app.decoder.decode(app.secrets["USER"]["password"])
   if user_password is False:
     print(".... Encrypt User Password ....")
-    user_password = settings["USER"]["password"]
-    settings["USER"]["password"] = secrets.encode(settings["USER"]["password"])
+    user_password = app.secrets["USER"]["password"]
+    app.secrets["USER"]["password"] = app.decoder.encode(app.secrets["USER"]["password"])
 
   with open("/media/usb/secrets.ini", "w") as configFile:
-    settings.write(configFile)
+    app.secrets.write(configFile)
 else:
-  wifi_password = settings["WIFI"]["password"]
-  user_password = settings["USER"]["password"]
+  wifi_password = app.secrets["WIFI"]["password"]
+  user_password = app.secrets["USER"]["password"]
 
 print("Setup wpa_supplicant.conf")
 wpa_supplicant_file = open("/etc/wpa_supplicant/wpa_supplicant.conf", "w")
 wpa_supplicant_file.write("ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n")
 wpa_supplicant_file.write("update_config=1\n")
-wpa_supplicant_file.write("country=" + settings["WIFI"]["country"]+"\n")
+wpa_supplicant_file.write("country=" + app.secrets["WIFI"]["country"]+"\n")
 wpa_supplicant_file.write("network={\n")
 wpa_supplicant_file.write("         ssid=\""+wifi_name+"\"\n")
 wpa_supplicant_file.write("         psk=\""+wifi_password+"\"\n")
 wpa_supplicant_file.write("}\n")
 wpa_supplicant_file.close()
 
-
 print(" Changing name to " + name)
-system.change_name(name)
+app.system.change_name(name)
 
 print(" Changing SSH password for " + user_name)
-system.update_ssh_user(user_name, user_password)
+print(user_password)
+app.system.change_ssh_user(user_name, user_password)
 
 print(" Changing SAMBA password for " + user_name)
-system.update_samba_user(user_name, user_password)
+app.system.change_samba_user(user_name, user_password)
+
+if app.settings["APPLICATION"]["type"] != app.system.info["current_app"]:
+  print("Changing application from " + app.system.info["current_app"] + " to " + app.settings["APPLICATION"]["type"])
+  app.system.change_app(app.settings["APPLICATION"]["type"])
 
 print(" Connecting to " + wifi_name)
-system.restart_wifi()
+app.system.change_wifi()
